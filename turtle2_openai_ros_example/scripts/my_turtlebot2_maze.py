b0457ef8815f8f6e351f077b893ca0308c32d2d7
RDAneelOlivaw <duckfrost@gmail.com>
2018-08-02 09:13:15 +0000

    Added new structure adaptations

--- turtle2_openai_ros_example/scripts/my_turtlebot2_maze.py
+++ turtle2_openai_ros_example/scripts/my_turtlebot2_maze.py
@@ -1,7 +1,7 @@
 import rospy
 import numpy
 from gym import spaces
-from openai_ros import turtlebot2_env
+from openai_ros.robot_envs import turtlebot2_env
 from gym.envs.registration import register
 
 timestep_limit_per_episode = 10000 # Can be any Value
